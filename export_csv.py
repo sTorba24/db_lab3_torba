181a4ab3ea3108532561cb5bea6d0d81d4533978
Sviatoslav <55200530+sTorba24@users.noreply.github.com>
2021-12-20 00:03:36 +0200

    Add files via upload

---
+++ export_csv.py
@@ -0,0 +1,30 @@
+import csv
+import psycopg2
+
+OUTPUT_FILE_T = 'torba_DB_{}.csv'
+
+TABLES = [
+    'humans',
+    'children',
+    'insuranse'
+]
+
+
+conn = psycopg2.connect(
+  host="localhost",
+  database="studentTorba",
+  user="postgres",
+  password="sobaka123")
+
+with conn:
+    cur = conn.cursor()
+
+    for table_name in TABLES:
+        cur.execute('SELECT * FROM ' + table_name)
+        fields = [x[0] for x in cur.description]
+
+        with open(OUTPUT_FILE_T.format(table_name), 'w') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(fields)
+            for row in cur:
+                writer.writerow([str(x).lstrip() for x in row])
